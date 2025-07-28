bf008e5799de5c89712611e5c2793659bf70c39d
sakurakyuo <sakurakyuo@protonmail.com>
2025-07-29 03:11:28 +0800

    sm8650-common: Move UdfpsExtension to Device Tree

--- common.mk
+++ common.mk
@@ -156,8 +156,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-$(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
-
 TARGET_HAS_UDFPS := true
 
 # GPS
